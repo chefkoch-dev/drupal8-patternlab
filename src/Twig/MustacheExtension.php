df1e8bc81c22c7ebd07a94748add24c6dbe60eb4
Olav Schettler <olav.schettler@chefkoch.de>
2016-02-10 09:11:46 +0100

    Added dependency on mustache

---
+++ src/Twig/MustacheExtension.php
@@ -0,0 +1,24 @@
+<?php
+namespace Chefkoch\Twig;
+
+class MustacheExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('component',
+                function ($templateUrl, $arguments)
+                {
+                    $template = file_get_contents($templateUrl);
+                    $m = new \Mustache_Engine;
+                    return $m->render($template, $arguments);
+                }
+            )
+        ];
+    }
+
+    public function getName()
+    {
+        return 'mustache';
+    }
+}
